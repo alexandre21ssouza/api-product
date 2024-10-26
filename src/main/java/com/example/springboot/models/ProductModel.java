6aa3259b4d87999414cce88a069e573c7242d096
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2024-10-26 01:11:37 -0300

    Adição das anotations de entidades e tabelas; Criação da chave primaria e adição dos atributo da entidade.

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,4 +1,19 @@
 package com.example.springboot.models;
 
-public class ProductModel {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB PRODUCTS")
+public class ProductModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal VALUE;
 }
